eb413ca371d0d735165d72e5ee6360040d63821b
James Novakowski <james.novakowski@gmail.com>
2016-11-05 19:14:30 -0400

    Added ImageMagick paperclip gem, and modified pin resources to accept an image upload.
    Provided validation for image file upload.
    
    Issue (ongoing) with Pin model has_attached_file.  Currently works, but want to permit styles to be defined on upload - currently this doesn't work, but it should.

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 
 gem 'devise', '~> 4.2'
+gem 'paperclip', '~> 5.0'
 
 # Use bootstrap-sass for CSS prettifying
 gem 'bootstrap-sass'
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,8 @@
 class Pin < ApplicationRecord
 	belongs_to :user
+
+	has_attached_file :image#, :styles => { :medium => "300x300#", :thumb => "100x100#" }
+
+	#paperclip requires the image item be validated.
+	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
