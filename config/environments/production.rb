4bf625b8896a343cbed1f024ad33935b170fd310
James Novakowski <james.novakowski@gmail.com>
2016-11-06 16:16:11 -0500

    Renamed ENV variable AWS_Bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,11 +89,11 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { host=> "http://pinteresting-nova.herokuapp.com" }
 
   #Sets paperclip to upload images to Amazon S3
-  
+
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    bucket: ENV.fetch('AWS_BUCKET'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
